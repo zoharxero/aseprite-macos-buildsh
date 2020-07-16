2f78931bcf8570604559a1bb8e65e54271b6f0d7
zoharxero <zoharxero@gmail.com>
2020-07-16 11:23:08 -0500

    Change skia gn flags to x64

--- aseprite.sh
+++ aseprite.sh
@@ -126,8 +126,8 @@ cd skia
 #SKIA_BRANCH=$(curl "https://raw.githubusercontent.com/aseprite/aseprite/master/INSTALL.md" | grep "aseprite-m[0-9][0-9]" | sed -n '1p' | perl -n -e '/(aseprite-m\d\d)/ && print $1')
 #git checkout $SKIA_BRANCH
 python tools/git-sync-deps
-gn gen out/Release --args="is_official_build=true skia_use_system_expat=false skia_use_system_icu=false skia_use_libjpeg_turbo=false skia_use_system_libpng=false skia_use_system_libwebp=false skia_use_system_zlib=false skia_use_libwebp=false extra_cflags_cc=[\"-frtti\"]"
-ninja -C out/Release skia
+gn gen out/Release-x64 --args="is_debug=false is_official_build=true skia_use_system_expat=false skia_use_system_icu=false skia_use_system_libjpeg_turbo=false skia_use_system_libpng=false skia_use_system_libwebp=false skia_use_system_zlib=false skia_use_sfntly=false skia_use_freetype=true skia_use_harfbuzz=true skia_pdf_subset_harfbuzz=true skia_use_system_freetype2=false skia_use_system_harfbuzz=false target_cpu=\"x64\" extra_cflags=[\"-stdlib=libc++\", \"-mmacosx-version-min=10.9\"] extra_cflags_cc=[\"-frtti\"]"
+ninja -C out/Release-x64 skia modules
 
 cd ../../
 
@@ -152,7 +152,7 @@ cmake \
   -DCMAKE_OSX_SYSROOT="$SDK_ROOT/$POSTFIXPATH_SDKROOT" \
   -DLAF_OS_BACKEND=skia \
   -DSKIA_DIR="${PWD}/../../deps/skia" \
-  -DSKIA_OUT_DIR="${PWD}/../../deps/skia/out/Release" \
+  -DSKIA_OUT_DIR="${PWD}/../../deps/skia/out/Release-x64" \
   -G Ninja \
   .. && \
 ninja aseprite # when finish, build file will be at aseprite/build/bin
